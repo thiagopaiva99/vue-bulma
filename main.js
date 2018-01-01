328b3c2345a92c878089b7572445f366ff9397e9
Thiago Galvani <thiagopaiva99@gmail.com>
2018-01-01 21:32:39 -0200

    Creating a simple component with Bulma

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+Vue.component('message', {
+    template: `
+        <article class="message">
+            <div class="message-header">
+                <p>Hello World</p>
+                <button class="delete"></button>
+            </div>
+            <div class="message-body">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nam unde, quae nihil sequi dolor explicabo aspernatur magnam vel veniam omnis ut atque doloribus ipsa est ipsam modi aliquam a.
+            </div>
+        </article>
+    `
+})
+
 new Vue({
     el: "#app"
 })
