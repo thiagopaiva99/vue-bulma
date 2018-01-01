0fffbd5907b5d4d865fc571904fa7f8e3dcf348a
Thiago Galvani <thiagopaiva99@gmail.com>
2018-01-01 21:36:19 -0200

    Hidding message with Vuejs

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 Vue.component('message', {
     template: `
-        <article class="message">
+        <article class="message" v-show="visible">
             <div class="message-header">
                 <p>Hello World</p>
-                <button class="delete"></button>
+                <button class="delete" @click="hiddeMessage"></button>
             </div>
             <div class="message-body">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nam unde, quae nihil sequi dolor explicabo aspernatur magnam vel veniam omnis ut atque doloribus ipsa est ipsam modi aliquam a.
             </div>
         </article>
-    `
+    `,
+    data: () => {
+        return {
+            visible: true
+        }
+    },
+    methods: {
+        hiddeMessage: function() {
+            this.visible = !this.visible
+        }
+    }
 })
 
 new Vue({
