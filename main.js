a59958e946be83224c162a7b62bea94b2aba312d
Thiago Galvani <thiagopaiva99@gmail.com>
2018-01-01 21:52:12 -0200

    Removing message Component

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-Vue.component('message', {
-    template: `
-        <article class="message" v-show="visible">
-            <div class="message-header">
-                <p>{{ title }}</p>
-                <button class="delete" @click="hiddeMessage"></button>
-            </div>
-            <div class="message-body">
-                {{ body }}
-            </div>
-        </article>
-    `,
-    data: () => {
-        return {
-            visible: true
-        }
-    },
-    props: [
-        'title',
-        'body'
-    ],
-    methods: {
-        hiddeMessage: function() {
-            this.visible = !this.visible
-        }
-    }
-})
-
 new Vue({
     el: "#app"
 })
