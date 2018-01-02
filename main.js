cd0218444219098a82204fb07ed9857f6ef9bf1d
Thiago Galvani <thiagopaiva99@gmail.com>
2018-01-01 22:07:40 -0200

    Opening and closing the Modal Component

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+            <!-- Any other Bulma elements you want -->
+            </div>
+            <button class="modal-close is-large" aria-label="close" @click="$emit('openModal')"></button>
+        </div>
+    `
+})
+
 new Vue({
-    el: "#app"
+    el: "#app",
+    data: () => {
+        return {
+            showModal: false
+        }
+    }
 })
